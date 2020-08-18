94bd55ff7a14f79bcb6fb4583710a159ff533454
Szymon Grzelak <szymong333@gmail.com>
2020-08-18 17:55:44 +0200

    Issue #1 - Add basic UI using Swing forms

---
+++ src/main/java/com/github/szymon2603/fixedlengthrecordseditor/ui/swing/FixedLengthRecordsEditor.java
@@ -0,0 +1,12 @@
+package com.github.szymon2603.fixedlengthrecordseditor.ui.swing;
+
+import javax.swing.*;
+
+public class FixedLengthRecordsEditor {
+    protected JTabbedPane tabbedPane;
+    protected JPanel recordsConfigTab;
+    protected JPanel recordsTableTab;
+    protected RecordsConfigForm recordsConfigForm;
+    protected RecordsTableForm recordsTableForm;
+    protected JTable configTable;
+}
