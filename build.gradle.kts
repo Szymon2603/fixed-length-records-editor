52a430639628487f297bc7852903a87e1de0850d
Szymon Grzelak <szymong333@gmail.com>
2020-11-18 23:03:55 +0100

    Remove Java support plugin

--- build.gradle.kts
+++ build.gradle.kts
@@ -4,8 +4,6 @@ import org.jetbrains.changelog.markdownToHTML
 import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 
 plugins {
-    // Java support
-    id("java")
     // Kotlin support
     id("org.jetbrains.kotlin.jvm") version "1.3.72"
     // gradle-intellij-plugin - read more: https://github.com/JetBrains/gradle-intellij-plugin
